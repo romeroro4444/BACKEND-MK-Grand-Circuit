76ac12ba59814ba6cc91195ce3295468e350c9e6
romeroro4444 <romerorodrigorojas@gmail.com>
2026-03-15 21:43:13 -0300

    Fix Swagger on Vercel using CDN + swagger.json endpoint

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./config/swagger");
 const authRoutes = require("./routes/auth.routes");
 const teamsRoutes = require("./routes/teams.routes");
@@ -12,7 +11,49 @@ const { authenticateJWT } = require("./middlewares/auth.middleware");
 const app = express();
 
 app.use(express.json());
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get("/api-docs/swagger.json", (req, res) => {
+  return res.status(200).json(swaggerSpec);
+});
+
+const swaggerHtml = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>MK Grand Circuit API Docs</title>
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css"
+    />
+  </head>
+  <body>
+    <div id="swagger-ui"></div>
+    <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+    <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-standalone-preset.js"></script>
+    <script>
+      window.onload = () => {
+        window.ui = SwaggerUIBundle({
+          url: '/api-docs/swagger.json',
+          dom_id: '#swagger-ui',
+          deepLinking: true,
+          presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+          layout: 'BaseLayout'
+        });
+      };
+    </script>
+  </body>
+</html>`;
+
+app.get("/api-docs", (req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  return res.status(200).send(swaggerHtml);
+});
+
+app.get("/api-docs/", (req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  return res.status(200).send(swaggerHtml);
+});
 
 app.get("/", (req, res) => {
   return res.json({ message: "API de Express funcionando" });
